5ba07ed011f7422c9bc22d8347a63018be8868e7
GoodBoyNeon <goodboyneon03@gmail.com>
2023-09-09 10:26:40 +0545

    chore(sentry): init sentry

--- environment.d.ts
+++ environment.d.ts
@@ -1,10 +1,12 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      BOT_TOKEN: string;
       NODE_ENV: 'production' | 'development';
+      BOT_TOKEN: string;
       DATABASE_URL: string;
+      SENTRY_DSN: string;
       GPT_API_KEY: string;
+      PALM_API_KEY: string;
     }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
 import { PrismaClient } from '@prisma/client';
 import { logger, setWizardConfig } from 'console-wizard';
-import { config } from 'dotenv';
+import * as dotenv from 'dotenv';
 import { Byte } from './lib';
+
+if (!process.env.NODE_ENV) {
+  logger.error('NODE_ENV not found!', { includeTimestamp: false });
+  process.exit(1);
+}
+
+dotenv.config({
+  path: `${__dirname}/../.env.${process.env.NODE_ENV}`,
+});
+
+// Sentry
+import * as Sentry from '@sentry/node';
+import { ProfilingIntegration } from '@sentry/profiling-node';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [new ProfilingIntegration()],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+});
 
 setWizardConfig({
   includeSN: true,
@@ -9,17 +29,7 @@ setWizardConfig({
   includeTimestamp: true,
 });
 
-if (!process.env.NODE_ENV) {
-  logger.error('NODE_ENV not found!', { includeTimestamp: false });
-  process.exit(1);
-}
-
-config({
-  path: `${__dirname}/../.env.${process.env.NODE_ENV}`,
-});
-
 export const client = new Byte();
-
 export const prisma = new PrismaClient();
 
 client.deploy();
